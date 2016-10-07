69461350ee733a66108fcf5b53cdafec71f929bf
Evelin Padjus <evelin@padjus.com>
2016-10-07 19:19:17 +0300

    Main Window

--- src/main/java/com/padjus/pwdman/Main.java
+++ src/main/java/com/padjus/pwdman/Main.java
@@ -1,3 +1,20 @@
+/*
+ *     Copyright (C) 2016 Evelin Padjus pwdman
+ *
+ *     pwdman is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.padjus.pwdman;
 
 /**
@@ -21,11 +38,12 @@ public class Main extends Application {
 	}
 
 	@Override
-	public void start(Stage stage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("pwdmanMainView" +
-															 ".fxml"));
+	public void start (Stage stage) throws Exception {
 
-		Scene scene = new Scene(root, 300, 275);
+		Parent root = FXMLLoader
+				.load(getClass().getResource("pwdmanMainView" + ".fxml"));
+
+		Scene scene = new Scene(root, 400, 275);
 
 		stage.setTitle("pwdman - Password Manager v1.0");
 		stage.setScene(scene);
